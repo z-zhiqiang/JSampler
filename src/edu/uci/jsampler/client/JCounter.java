4cd7fcc7a461e17dd43d9c23a6c1e230da0bebff
z_zhiqiang <z_zhiqiang@localhost>
2015-10-08 18:02:40 -0700

    incomplete update

---
+++ src/edu/uci/jsampler/client/JCounter.java
@@ -0,0 +1,127 @@
+package edu.uci.jsampler.client;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import edu.uci.jsampler.transformer.PCounter;
+import soot.PackManager;
+import soot.Transform;
+import soot.options.Options;
+
+public class JCounter {
+	public static final String counts_output = "./output.counts";
+	
+	// instrumentation flag
+	private static boolean branches_flag;
+
+	private static boolean returns_flag;
+
+	private static boolean scalarpairs_flag;
+
+	private static boolean methodentries_flag;
+
+
+	// methods instrumented
+	private static Set<String> methods_instrument;
+
+	
+	public static void main(String[] args) {
+		//parse arguments
+		List<String> soot_parameters = new ArrayList<String>();
+		parseParameters(args, soot_parameters);
+
+		PCounter counter = new PCounter(branches_flag, returns_flag, scalarpairs_flag, methodentries_flag, methods_instrument);
+		PackManager.v().getPack("jtp").add(new Transform("jtp.counter", counter));
+
+		Options.v().setPhaseOption("jb", "use-original-names:true");
+		Options.v().set_output_format(Options.output_format_jimple);
+		Options.v().set_keep_line_number(true);
+		Options.v().set_prepend_classpath(true);
+
+		soot.Main.main(soot_parameters.toArray(new String[soot_parameters.size()]));
+
+		// export static instrumentation information into files
+		exportCounts();
+		
+	}
+
+	private static void exportCounts() {
+		File file = new File(counts_output);
+		PrintWriter out = null;
+		try {
+			if (!file.getParentFile().exists()) {
+				file.getParentFile().mkdirs();
+			}
+			out = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+
+			out.printf("Branches=%d\nReturns=%d\nScalarPairs=%d\nMethodEntries=%d", PCounter.counts_branch, PCounter.counts_return, PCounter.counts_scalarPair, PCounter.counts_methodEntry);
+			
+			out.close();
+		} catch(FileNotFoundException e){ 
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(out != null){
+				out.close();
+			}
+		}
+	}
+
+	/**
+	 * parse parameters into sampler-options and soot-parameters
+	 * 
+	 * @param args
+	 * @param soot_parameters
+	 */
+	private static void parseParameters(String[] args, List<String> soot_parameters) {
+		if (args.length == 0) {
+			System.err.println("Usage: java JCounter sampler-options [soot-options] classname");
+			System.exit(0);
+		}
+		
+		methods_instrument = new HashSet<String>();
+		
+		// TODO Auto-generated method stub
+		for (int i = 0; i < args.length; i++) {
+			String option = args[i].trim();
+			if (option.startsWith("-sampler")) {
+				if (option.equals("-sampler-scheme=branches")) {
+					branches_flag = true;
+				} 
+				else if (option.equals("-sampler-scheme=returns")) {
+					returns_flag = true;
+				} 
+				else if (option.equals("-sampler-scheme=scalar-pairs")) {
+					scalarpairs_flag = true;
+				} 
+				else if (option.equals("-sampler-scheme=method-entries")) {
+					methodentries_flag = true;
+				} 
+				else if (option.equals("-sampler")) {
+				} 
+				else if (option.startsWith("-sampler-include-method=")) {
+					methods_instrument.add(option.split("=")[1].trim());
+				} 
+				else if (option.startsWith("-sampler-out-sites=")) {
+				}
+				else{
+					System.err.println("wrong option!");
+				}
+			} else {
+				soot_parameters.add(option);
+			}
+		}
+	}
+	
+
+}
---
+++ src/edu/uci/jsampler/transformer/PCounter.java
@@ -0,0 +1,107 @@
+package edu.uci.jsampler.transformer;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import soot.Body;
+import soot.BodyTransformer;
+import soot.Local;
+import soot.PrimType;
+import soot.Unit;
+import soot.Value;
+import soot.jimple.AssignStmt;
+import soot.jimple.IdentityStmt;
+import soot.jimple.IfStmt;
+import soot.jimple.Stmt;
+import soot.toolkits.graph.BriefUnitGraph;
+import soot.toolkits.scalar.FlowSet;
+import soot.toolkits.scalar.InitAnalysis;
+import soot.util.Chain;
+
+public class PCounter extends BodyTransformer {
+	
+	// instrumentation flag
+	private final boolean branches_flag;
+
+	private final boolean returns_flag;
+
+	private final boolean scalarpairs_flag;
+
+	private final boolean methodentries_flag;
+
+	private final Set<String> methods_instrument;// methods instrumented
+	
+	// counts
+	public static int counts_branch;
+	
+	public static int counts_return;
+	
+	public static int counts_scalarPair;
+	
+	public static int counts_methodEntry;
+	
+
+	public PCounter(boolean branches_flag, boolean returns_flag, boolean scalarpairs_flag, boolean methodentries_flag, Set<String> methods_instrument) {
+		// TODO Auto-generated constructor stub
+		this.branches_flag = branches_flag;
+		this.returns_flag = returns_flag;
+		this.scalarpairs_flag = scalarpairs_flag;
+		this.methodentries_flag = methodentries_flag;
+		this.methods_instrument = methods_instrument;
+	}
+
+	@Override
+	protected void internalTransform(Body body, String phaseName, Map options) {
+		// get body's units
+		Chain<Unit> units = body.getUnits();
+		Iterator<Unit> original_stmtIt = units.snapshotIterator();
+		
+		//initialized variables
+		InitAnalysis analysis = new InitAnalysis(new BriefUnitGraph(body));
+		
+		boolean under_analysis = this.methods_instrument.isEmpty() || this.methods_instrument.contains(body.getMethod().getSignature());		
+		
+		//instrument the specified methods
+		if(under_analysis){
+			boolean instrumentEntry_flag = true;
+			Value def = null;
+			
+			while (original_stmtIt.hasNext()) {
+				// cast back to a statement
+				Stmt stmt = (Stmt) original_stmtIt.next();
+				
+				// for method-entries
+				if (this.methodentries_flag && instrumentEntry_flag && !(stmt instanceof IdentityStmt)) {
+					counts_methodEntry++;
+					instrumentEntry_flag = false;
+				}
+				
+				// for branches
+				if (this.branches_flag && stmt instanceof IfStmt) {
+					counts_branch++;
+				}
+				// for returns and scalar-pairs
+				if (stmt instanceof AssignStmt && (def = ((AssignStmt) stmt).getLeftOp()).getType() instanceof PrimType) {
+					// for returns
+					if (this.returns_flag && ((Stmt) stmt).containsInvokeExpr()) {
+						counts_return++;
+					}
+					// for scalar-pairs
+					else if(this.scalarpairs_flag){
+						Iterator<Local> it = ((FlowSet) analysis.getFlowAfter(stmt)).iterator();
+						while(it.hasNext()){
+							Local local = (Local) it.next();
+							if (local.getType() == def.getType() && local != def) {
+								counts_scalarPair++;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	
+
+}
